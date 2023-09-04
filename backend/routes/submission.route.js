b054fab366f1c6ca9542c9655794f73abf74663f
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-09-04 20:32:04 +0530

    added a route to get thesubmissions based on course

--- backend/routes/submission.route.js
+++ backend/routes/submission.route.js
@@ -40,6 +40,54 @@ submissionRouter.post(
   }
 );
 
+// -- get the submissions based on course(id)
+submissionRouter.get(
+  "/api/submission/course/:id",
+  authentication,
+  authorize(["admin", "instructor"]),
+  async (req, res) => {
+    try {
+      Assignment.hasMany(Submission, { foreignKey: "assignment_id" });
+      Submission.belongsTo(Student, { foreignKey: "assignment_id" });
+
+      Student.hasMany(Enrollment, { foreignKey: "student_id" });
+      Enrollment.belongsTo(Student, { foreignKey: "student_id" });
+
+      Course.hasMany(Enrollment, { foreignKey: "course_id" });
+      Enrollment.belongsTo(Course, { foreignKey: "course_id" });
+
+      Course.hasMany(Assignment, { foreignKey: "course_id" });
+      Assignment.belongsTo(Course, { foreignKey: "course_id" });
+
+      const query = `
+              SELECT s.name, sb.*
+              FROM submissions sb
+              JOIN assignments a ON sb.assignment_id = a.id
+              JOIN courses c ON a.course_id = c.id
+              JOIN enrollments e ON c.id = e.course_id
+              JOIN students s ON e.student_id = s.id
+              WHERE c.id = :studentId
+              ORDER BY sb.submission_date DESC;
+          `;
+
+      const submissionsOfACourse = await sequelize.query(query, {
+        type: Sequelize.QueryTypes.SELECT,
+        replacements: { studentId: req.params.id }, // Replace with the actual student ID
+      });
+
+      if (submissionsOfACourse.length === 0)
+        return res.json({
+          message: "No submissions available.",
+        });
+
+      res.json(submissionsOfACourse);
+    } catch (err) {
+      console.error("Error fetching submissions:", err);
+      res.send({ error: err.message });
+    }
+  }
+);
+
 module.exports = {
   submissionRouter,
 };
