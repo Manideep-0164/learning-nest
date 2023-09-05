2cd98115da71b812a79388da6f7b3c92f9cf1012
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-09-05 13:40:00 +0530

    added department route, includes two endpoints one for posting department and one for getting departmetns along with the courses

---
+++ backend/routes/department.route.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { Assignment } = require("../models/assignment.model");
+const { Course } = require("../models/course.model");
+const { Enrollment } = require("../models/enrollment.model");
+const { Student } = require("../models/student.model");
+const { Submission } = require("../models/submissions.model");
+const { sequelize } = require("../configs/db");
+const { Sequelize } = require("sequelize");
+const { Instructor } = require("../models/instructor.model");
+const { Department } = require("../models/department.model");
+const { authentication } = require("../middlewares/authentication.middleware");
+const { authorize } = require("../middlewares/authorization.middleware");
+const departmentRouter = express.Router();
+
+departmentRouter.post(
+  "/api/department",
+  authentication,
+  authorize(["admin"]),
+  async (req, res) => {
+    try {
+      const { name } = req.body;
+      const deptData = await Department.create({
+        name,
+      });
+      res.json({ "Department Created": deptData });
+    } catch (err) {
+      console.error("Error fetching departments:", err);
+      res.send({ error: err.message });
+    }
+  }
+);
+
+// get courses along with depts
+departmentRouter.get("/api/department/courses", async (req, res) => {
+  try {
+    Department.hasMany(Course, { foreignKey: "dept_id" });
+    Course.belongsTo(Department, { foreignKey: "dept_id" });
+
+    const departmentCourses = await Department.findAll({
+      attributes: ["name"],
+      include: {
+        model: Course,
+        as: "courses",
+        attributes: ["id", "name", "description"], // Select the attributes you want
+      },
+    });
+
+    if (departmentCourses.length === 0)
+      return res.status(404).json({ message: "No departments exists." });
+
+    res.json(departmentCourses);
+  } catch (err) {
+    console.error("Error fetching courses:", err);
+    res.send({ error: err.message });
+  }
+});
+
+module.exports = {
+  departmentRouter,
+};
