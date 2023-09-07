a7e26f6d4945da524d3cb2c5709ba4b7b05ba61b
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-09-07 23:36:29 +0530

    changes made in the navbar, footer and instructor components

--- frontend/src/app/chatbot/chatbot.component.ts
+++ frontend/src/app/chatbot/chatbot.component.ts
@@ -1,4 +1,11 @@
-import { Component,ViewChild, ElementRef, AfterViewChecked,Input,OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewChecked,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { GlobalService } from '../global.service';
 import { AIService } from '../ai.service';
 import { promptForBot } from '../Allmodels.model';
@@ -8,72 +15,61 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-chatbot',
   templateUrl: './chatbot.component.html',
-  styleUrls: ['./chatbot.component.css']
+  styleUrls: ['./chatbot.component.css'],
 })
-export class ChatbotComponent implements AfterViewChecked , OnInit {
+export class ChatbotComponent implements AfterViewChecked, OnInit {
   @ViewChild('chatContainer') private chatContainer!: ElementRef;
   @Input() data: any;
-  chatMessages: { text: string, isUser: boolean }[] = [];
-  constructor (private globalservice : GlobalService,private AiService : AIService,private http: HttpClient) {}
+  chatMessages: { text: string; isUser: boolean }[] = [];
+  constructor(
+    private globalservice: GlobalService,
+    private AiService: AIService,
+    private http: HttpClient
+  ) {}
 
   visible: boolean = false;
- 
-  
+
   ngOnInit(): void {
-    this.chatMessages.push({text :`Hello! ${this.data || 'User'}`,isUser:false},{text :'Welcome to EduHub',isUser:false},{text :'How can I assist you today?',isUser:false})
-
-
-    
-
-
+    this.chatMessages.push(
+      { text: `Hello! ${this.data || 'User'}`, isUser: false },
+      { text: 'Welcome to LearningNest', isUser: false },
+      { text: 'How can I assist you today?', isUser: false }
+    );
   }
-  
-  
-  
-  
-  
-  
-      showDialog() {
-       
-          this.visible = true;
-      }
-    
-    async sendMessage(): Promise<void> {
-    
-    const userMessage = document.querySelector('.chat-input input') as HTMLInputElement;
-    let messageText = userMessage.value.trim();
-   
 
-    
+  showDialog() {
+    this.visible = true;
+  }
+
+  async sendMessage(): Promise<void> {
+    const userMessage = document.querySelector(
+      '.chat-input input'
+    ) as HTMLInputElement;
+    let messageText = userMessage.value.trim();
+
     if (messageText) {
       this.chatMessages.push({ text: messageText, isUser: true });
 
       // Simulate a bot response (you can replace this with actual chatbot logic)
       userMessage.value = '';
 
-      
-      
-
       let messageForBot = {
-        message : messageText
-      }
-
+        message: messageText,
+      };
 
       console.log(messageText);
-      
-      
-      let botResponse ="";
-       this.http.post(`${environment.API_URL}/api/ai/chat`, messageForBot).subscribe((res:any)=>{
-        console.log('response',res);
-        botResponse = res?.message
-        this.chatMessages.push({ text:  res?.message, isUser: false });
-        
-       })
-        
-       
+
+      let botResponse = '';
+      this.http
+        .post(`${environment.API_URL}/api/ai/chat`, messageForBot)
+        .subscribe((res: any) => {
+          console.log('response', res);
+          botResponse = res?.message;
+          this.chatMessages.push({ text: res?.message, isUser: false });
+        });
 
       userMessage.value = '';
-      messageText = "";
+      messageText = '';
     }
   }
 
@@ -83,8 +79,8 @@ export class ChatbotComponent implements AfterViewChecked , OnInit {
 
   private scrollToBottom(): void {
     try {
-      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+      this.chatContainer.nativeElement.scrollTop =
+        this.chatContainer.nativeElement.scrollHeight;
     } catch (err) {}
   }
-
 }
