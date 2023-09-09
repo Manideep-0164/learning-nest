45ce28d932b7b06c956b0a31b0f28bfa41b8f9d3
Manideep-0164 <peddaboinimanideep03@gmail.com>
2023-09-09 22:55:13 +0530

    increased the width of the signup components

--- frontend/src/app/single-assignment/single-assignment.component.ts
+++ frontend/src/app/single-assignment/single-assignment.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GlobalService } from '../global.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
@@ -10,83 +10,95 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-single-assignment',
   templateUrl: './single-assignment.component.html',
-  styleUrls: ['./single-assignment.component.css']
+  styleUrls: ['./single-assignment.component.css'],
+  providers: [MessageService],
 })
 export class SingleAssignmentComponent implements OnInit {
+  isEdit: boolean = false;
+  assigment_date: string = '';
+  assignment: any = {};
+  assignment_id: number | undefined;
+  submittedData: string = '';
 
-  isEdit : boolean = false;
-  assigment_date : string ="";
-  assignment :any = {};
-  assignment_id : number | undefined;
-  submittedData : string ="";
-
-
-  constructor(private http: HttpClient, private route: ActivatedRoute, private globalService: GlobalService, private sanitizer: DomSanitizer, private messageService: MessageService, private confirmationService: ConfirmationService,private router: Router,private datePipe: DatePipe) { }
-
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private globalService: GlobalService,
+    private sanitizer: DomSanitizer,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private router: Router,
+    private datePipe: DatePipe
+  ) {}
 
   ngOnInit(): void {
-
-    this.getAssignemnts()
+    this.getAssignemnts();
   }
 
   sanitizeAndTrustHtml(html: string): SafeHtml {
     return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
-  getAssignemnts(){
+  getAssignemnts() {
     const token = this.globalService.getStudentLoginDetails()?.token ?? '';
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': token
+      Authorization: token,
     });
     const assignmentId = this.route.snapshot.params['id'];
-    this.assignment_id = assignmentId
-    this.http.get(`${environment.API_URL}/api/assignment/${assignmentId}/student/${this.globalService.getStudentLoginDetails()?.id}`, { headers }).subscribe((res:any) => {
-      
-      console.log(res);
-      this.assignment = res[0];
-      this.assigment_date = res[0]?.due_date;
-      this.submittedData = res[0]?.submittedData
-      
-    })
+    this.assignment_id = assignmentId;
+    this.http
+      .get(
+        `${environment.API_URL}/api/assignment/${assignmentId}/student/${
+          this.globalService.getStudentLoginDetails()?.id
+        }`,
+        { headers }
+      )
+      .subscribe((res: any) => {
+        console.log(res);
+        this.assignment = res[0];
+        this.assigment_date = res[0]?.due_date;
+        this.submittedData = res[0]?.submittedData;
+      });
   }
 
-
-  handleAddSubmission(){
+  handleAddSubmission() {
     const token = this.globalService.getStudentLoginDetails()?.token ?? '';
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': token
+      Authorization: token,
     });
     let submission = {
-      submission_date : this.datePipe.transform(new Date, 'yyyy-MM-dd'),
+      submission_date: this.datePipe.transform(new Date(), 'yyyy-MM-dd'),
       status: this.getSubmissionStatus(this.assigment_date),
-      assignment_id : this.assignment_id,
+      assignment_id: this.assignment_id,
       student_id: this.globalService.getStudentLoginDetails()?.id,
-      submittedData : this.submittedData,
-    }
-    
-    this.http.post(`${environment.API_URL}/api/submission`,submission,{headers}).subscribe((res)=>{
-      console.log(res);
-      
-    })
-    
+      submittedData: this.submittedData,
+    };
+
+    this.http
+      .post(`${environment.API_URL}/api/submission`, submission, { headers })
+      .subscribe((res) => {
+        console.log(res);
+        this.messageService.add({
+          key: 'tc',
+          severity: 'success',
+          summary: 'Submitted',
+          detail: 'Assignment successfully submitted',
+        });
+      });
   }
 
-  getSubmissionStatus(dueDateString:string) {
+  getSubmissionStatus(dueDateString: string) {
     const dueDate = new Date(dueDateString);
     const submissionDate = new Date();
-  
+
     if (submissionDate > dueDate) {
-      return "Late Submitted"; 
+      return 'Late Submitted';
     } else if (submissionDate <= dueDate) {
-      return "Submitted"; 
+      return 'Submitted';
     }
 
-    return "Not Submitted";
+    return 'Not Submitted';
   }
-
-
-
-
 }
